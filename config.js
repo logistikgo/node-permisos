42bf548e612b524a8dab667ce8bb55d1deb47095
imorales-logistikgo <imorales@logistikgo.com>
2020-04-30 10:45:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   // db: process.env.MONGODB || 'mongodb://40.124.41.46:27017/LogistikGO_XD',
   db:
     process.env.MONGODB ||
-    "mongodb://admin:2zaN=sR8c569%U9e4<K@ds131902.mlab.com:31902/logistikgo_xd_demo"
+    "mongodb://admin:R9VdEsuAXRY3@ds131902.mlab.com:31902/logistikgo_xd_demo"
   //   db:
   // process.env.MONGODB ||
   // `mongodb://admin:2zaN=sR8c569%U9e4<K@ds129233.mlab.com:29233/logistikgo_xd`
