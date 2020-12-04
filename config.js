f83a7aeb6f9a80a65be44e33f21f3f8d83c7034b
imorales-logistikgo <imorales@logistikgo.com>
2020-12-04 10:58:42 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //  port: process.env.PORT || 8080,
+    port: process.env.PORT || 8080,
   // db: process.env.MONGODB || 'mongodb://40.124.41.46:27017/LogistikGO_XD',
   // db:
   //  process.env.MONGODB ||
