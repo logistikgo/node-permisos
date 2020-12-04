438d0053995d92e2ba8fb6c258704eb5ff349f00
imorales-logistikgo <imorales@logistikgo.com>
2020-12-04 10:52:39 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,10 @@ module.exports = {
   // db:
   //  process.env.MONGODB ||
   //  "mongodb://admin:R9VdEsuAXRY3@ds131902.mlab.com:31902/logistikgo_xd_demo"
+   db:
+    process.env.MONGODB ||
+    "mongodb://almacen-demo:IXFmu0DCabtvPNuhFMyaXt9330USdhCNXuVmbPb2nH0SF3J4Fsv7XZ7KmV3t8v2rRwFCOvDId50bEhrrnE8VSQ==@almacen-demo.mongo.cosmos.azure.com:10255/LogistikGO_LKMXD_Demo"
+  
   //   db:
   // process.env.MONGODB ||
   // `mongodb://admin:2zaN=sR8c569%U9e4<K@ds129233.mlab.com:29233/logistikgo_xd`
