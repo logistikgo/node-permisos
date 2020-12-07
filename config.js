19c1cb11b05b29fd42561e8e47ed7e492f8bbbf7
imorales-logistikgo <imorales@logistikgo.com>
2020-12-07 09:49:28 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,12 @@ module.exports = {
   //  "mongodb://admin:R9VdEsuAXRY3@ds131902.mlab.com:31902/logistikgo_xd_demo"
    db:
     process.env.MONGODB ||
-    "mongodb://almacen-demo:IXFmu0DCabtvPNuhFMyaXt9330USdhCNXuVmbPb2nH0SF3J4Fsv7XZ7KmV3t8v2rRwFCOvDId50bEhrrnE8VSQ%3D%3D@almacen-demo.mongo.cosmos.azure.com:10255/LogistikGO_LKMXD_Demo?ssl=true&appName=@almacen-demo@"
+    //"mongodb://almacen-demo:IXFmu0DCabtvPNuhFMyaXt9330USdhCNXuVmbPb2nH0SF3J4Fsv7XZ7KmV3t8v2rRwFCOvDId50bEhrrnE8VSQ%3D%3D@almacen-demo.mongo.cosmos.azure.com:10255/LogistikGO_LKMXD_Demo?ssl=true&appName=@almacen-demo@"
     //"mongodb://almacen-demo:IXFmu0DCabtvPNuhFMyaXt9330USdhCNXuVmbPb2nH0SF3J4Fsv7XZ7KmV3t8v2rRwFCOvDId50bEhrrnE8VSQ==@almacen-demo.mongo.cosmos.azure.com:10255/LogistikGO_LKMXD_Demo"
   
   //   db:
   // process.env.MONGODB ||
+    "mongodb://almacen-demo:IXFmu0DCabtvPNuhFMyaXt9330USdhCNXuVmbPb2nH0SF3J4Fsv7XZ7KmV3t8v2rRwFCOvDId50bEhrrnE8VSQ%3D%3D@almacen-demo.mongo.cosmos.azure.com:10255/LogistikGO_LKMXD_Demo?ssl=true&appName=@almacen-demo@"
   // `mongodb://admin:2zaN=sR8c569%U9e4<K@ds129233.mlab.com:29233/logistikgo_xd`
 };
 
